0fde741e6681e60189a3bbae0a69c25638e450f5
agopalkr <agopalkr@ucsd.edu>
2024-10-08 22:41:29 +0000

    aries

--- 3D-tracking-scripts/run_aries_3d_track.sh
+++ 3D-tracking-scripts/run_aries_3d_track.sh
@@ -6,5 +6,5 @@ for job_index in {0..0}
 do  
     start=$(( (1024 / 4) * job_index ))
     end=$(( (1024 / 4) * (job_index + 1) -1))
-    aries run ag-${JOB}-${job_index} -j 1 -g 1 lingzhan/openvla -- bash -c "nvidia-smi && git clone https://github.com/akshaygopalkr/ml-depth-pro.git && git pull && cd ml-depth-pro && ${INSTALL} && ${RUN} ${start} ${end} ${job_index}"
+    aries run ag-${JOB}-${job_index} -j 1 -g 1 lingzhan/openvla -- bash -c "nvidia-smi && git clone https://github.com/akshaygopalkr/ml-depth-pro.git && cd ml-depth-pro && git pull && ${INSTALL} && ${RUN} ${start} ${end} ${job_index}"
 done

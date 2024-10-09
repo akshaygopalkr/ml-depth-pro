c503067de65313661b96c81842414cf0bfcf8ed0
akshaygopalkr <akshaygopalkrishnan@Akshays-MacBook-Pro.local>
2024-10-08 17:06:09 -0700

    Decreased batch size

--- 3D-tracking-scripts/run_aries_3d_track.sh
+++ 3D-tracking-scripts/run_aries_3d_track.sh
@@ -2,7 +2,7 @@ JOB="rt1-3D-track-key"
 INSTALL="pip install -e . && source get_pretrained_models.sh && pip install --upgrade torch torchvision torchaudio"
 RUN="bash 3D-tracking-scripts/save_3d_tracking_data.sh" 
 
-for job_index in {0..0}
+for job_index in {0..3}
 do  
     start=$(( (1024 / 4) * job_index ))
     end=$(( (1024 / 4) * (job_index + 1) -1))
